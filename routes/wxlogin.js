285b764367e32c3dfb6b5515b2d811f3b154c97a
diken666 <diken666@163.com>
2020-08-05 15:01:37 +0800

    接收昵称和头像

--- routes/wxlogin.js
+++ routes/wxlogin.js
@@ -4,19 +4,19 @@ const request = require("request")
 const wxConfig = require("../wxconfig/config")
 
 router.post('/', (req, res, next) => {
-  let { code } = req.body
-  console.log(code)
-  console.log(wxConfig.appId, wxConfig.appSecret)
+  let { code, name, avatar } = req.body
+  // console.log(code, name, avatar)
+  console.log(req.headers.token)
   if (code) {
     request.get({
       url: `https://api.weixin.qq.com/sns/jscode2session?appid=${wxConfig.appId}&secret=${wxConfig.appSecret}&js_code=${code}&grant_type=authorization_code`
     }, (err, response, body) => {
       if (response.statusCode === 200) {
-        let { openid } = JSON.parse(body)
-        console.log(openid)
+        let { openid, errmsg } = JSON.parse(body)
+        // console.log(openid, response)
         res.json({
           code: 1,
-          msg: "登录成功"
+          msg: errmsg || "登录成功"
         })
       } else {
         res.json({
